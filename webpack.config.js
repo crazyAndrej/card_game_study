b30ae00ba92cc98be68f48c8fd35196a922ee07b
Andrej Lukasevic <forandrej@gmail.com>
2022-11-01 19:36:20 +0000

    try-to-fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const LinkTypePlugin =
+    require('html-webpack-link-type-plugin').HtmlWebpackLinkTypePlugin
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
@@ -8,7 +10,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
-    entry: '/script.js',
+    entry: './src/index.js',
     mode: isProduction ? 'production' : 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,8 +20,35 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    'style-loader',
+                    'css-loader',
+                    'resolve-url-loader',
+                    {
+                        loader: 'sass-loader?sourceMap',
+                        options: {
+                            includePaths: [],
+                            sourceMap: true,
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -45,5 +74,108 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: '/index.html',
         }),
+        new LinkTypePlugin({
+            '*.css': 'text/css',
+        }),
     ],
 }
+
+// const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
+
+// const isProduction = process.env.NODE_ENV === 'production'
+
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+// const mode =
+//     process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+// module.exports = {
+//     entry: '/script.js',
+//     mode,
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'bundle.js',
+//         clean: true,
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.scss$/,
+//                 use: [
+//                     {
+//                         loader: MiniCssExtractPlugin.loader,
+//                         options: {},
+//                     },
+//                     'style-loader',
+//                     'css-loader',
+//                     'resolve-url-loader',
+//                     {
+//                         loader: 'sass-loader?sourceMap',
+//                         options: {
+//                             includePaths: [],
+//                             sourceMap: true,
+//                         },
+//                     },
+//                 ],
+//                 exclude: /node_modules/,
+//             },
+//             {
+//                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//                 type: 'asset/resource',
+//             },
+//             {
+//                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
+//                 type: 'asset/resource',
+//             },
+//         ],
+//     },
+//     optimization: {
+//         minimizer: ['...', new CssMinimizerPlugin()],
+//     },
+//     devtool:
+//         process.env.NODE_ENV === 'production' ? 'source-map' : 'source-map',
+
+//     plugins: [
+//         new CopyPlugin({
+//             patterns: [{ from: 'img', to: 'static' }],
+//         }),
+//         new MiniCssExtractPlugin(),
+//         new HtmlWebpackPlugin({
+//             template: '/index.html',
+//         }),
+//     ],
+// }
+
+// module.exports = {
+//     entry: './src/index.js',
+//     mode: isProduction ? 'production' : 'development',
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'bundle.js',
+//         clean: true,
+//     },
+//     module: {
+//         rules: [
+//             { test: /\.css$/, use: ['style-loader', 'scss-loader'] },
+//             {
+//                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//                 type: 'asset/resource',
+//             },
+//             {
+//                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
+//                 type: 'asset/resource',
+//             },
+//         ],
+//     },
+//     plugins: [
+//         new CopyPlugin({
+//             patterns: [{ from: 'img', to: 'static' }],
+//         }),
+//         new HtmlWebpackPlugin({
+//             template: './index.html',
+//         }),
+//     ],
+// }
